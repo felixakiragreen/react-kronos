27d24fcd2843c716dfd9bc2e01a531313696b293
Luke Dubert <luke@bluebridgedigital.com>
2015-03-31 17:23:39 -0400

    add dist files and build scripts

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,42 +1,50 @@
-var path = require('path');
-var webpack = require('webpack');
+var webpack = require('webpack')
 
-module.exports = {
-  devtool: 'source-map',
-  entry: './index',
-  output:
-    { path: __dirname
-    , filename: 'bundle.js'
-    , publicPath: '/'
+
+module.exports =
+{ devtool: 'source-map'
+, entry: './index'
+, output:
+  { path: __dirname
+  , filename: 'bundle.js'
+  , publicPath: '/'
+}
+, resolveLoader:
+  { modulesDirectories: [ 'node_modules' ]
   }
-  , resolveLoader:
-    { modulesDirectories: ['node_modules']
+, resolve:
+  { extensions: ['', '.js', '.coffee', '.cjsx']
+  }
+, module:
+  { loaders:
+  [ { test: /\.json$/
+    , loader: 'json'
     }
-  , resolve: {
-    extensions: ['', '.js', '.coffee', '.cjsx']
-  },
-  module: {
-    loaders:
-    [ { test: /\.json$/, loader: 'json' }
-    , { test: /\.js?$/
-      , loader: 'babel-loader'
-      , include: path.join(__dirname, './')
-      }
-    , { test: /\.cjsx$/, loaders: ['coffee', 'cjsx'] }
-    , { test: /\.coffee$/, loader: 'coffee' }
-    ]
-  },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      }
-    })
+  , { test: /\.jsx?$/
+    , loader: 'babel'
+    , include: __dirname
+    , exclude: /node_modules/
+    }
+  , { test: /\.cjsx$/
+    , loaders: ['coffee', 'cjsx']
+    }
+  , { test: /\.coffee$/
+    , loader: 'coffee'
+    }
   ]
-};
+}
+, plugins:
+  [ new webpack.NormalModuleReplacementPlugin(/^kronos$/, '../src/index')
+  , new webpack.optimize.OccurenceOrderPlugin()
+  , new webpack.DefinePlugin(
+    { 'process.env':
+      { 'NODE_ENV': JSON.stringify('production') }
+    }
+  )
+  , new webpack.optimize.UglifyJsPlugin(
+    { compressor:
+      { warnings: false }
+    }
+  )
+  ]
+}
