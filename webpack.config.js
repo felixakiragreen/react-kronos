ec61377684e70a3504544766e58a35106347e791
Luke Dubert <luke@bluebridgedigital.com>
2015-04-03 19:45:19 -0400

    update min build

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ var webpack = require('webpack')
 
 
 module.exports =
-{ entry: './src/index'
+{ entry: './dist/index'
 , output:
   { path: __dirname + '/dist'
   , filename: 'kronos.min.js'
   , publicPath: '/'
   }
 , resolve:
-  { extensions: ['', '.js', '.coffee', '.cjsx']
+  { extensions: ['', '.js']
   }
 , module:
   { loaders:
@@ -20,17 +20,10 @@ module.exports =
       , loader: 'babel'
       , exclude: /node_modules/
       }
-    , { test: /\.cjsx$/
-      , loaders: ['coffee', 'cjsx']
-      }
-    , { test: /\.coffee$/
-      , loader: 'coffee'
-      }
     ]
   }
 , plugins:
-  [ new webpack.NormalModuleReplacementPlugin(/^kronos$/, '../src/index')
-  , new webpack.optimize.OccurenceOrderPlugin()
+  [ new webpack.optimize.OccurenceOrderPlugin()
   , new webpack.DefinePlugin(
     { 'process.env':
       { 'NODE_ENV': JSON.stringify('production') }
