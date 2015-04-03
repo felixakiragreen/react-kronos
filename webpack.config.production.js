15b607e19cb01281600bebe3dab452343f96f889
Luke Dubert <luke@bluebridgedigital.com>
2015-04-03 19:40:11 -0400

    prep for 0.1.0 release

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -23,7 +23,7 @@ module.exports =
     }
   , { test: /\.jsx?$/
     , loader: 'babel'
-    , include: __dirname
+    , include: [ __dirname, path.join(__dirname, '../src') ]
     , exclude: /node_modules/
     }
   , { test: /\.cjsx$/
