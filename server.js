2b444e3a405f199507cab74ee21001c17758627c
Luke Dubert <luke@bluebridgedigital.com>
2015-09-14 13:26:56 -0400

    scroll hour view, fix min date bug

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ var config = require('./webpack.config');
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true
+  historyApiFallback: true,
+  stats: {
+    colors: true,
+  }
 }).listen(3373, 'localhost', function (err, result) {
   if (err) {
     console.log(err);
