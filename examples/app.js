3b1f78496da89f199218ca78e08f8fde09e69b36
Luke Dubert <luke@bluebridgedigital.com>
2017-01-24 11:36:43 -0500

    update to 1.5.0

--- examples/app.js
+++ examples/app.js
@@ -120,7 +120,7 @@ export default class App extends Component {
         <div style={style.page}>
           <header style={style.header}>
             <div>
-              <h1 style={{ margin: 0 }}>React Kronos 1.4.2</h1>
+              <h1 style={{ margin: 0 }}>React Kronos 1.5.0</h1>
               <span style={style.tagline}>A fast, intuitive, and elegant date and time picker for React.</span>
             </div>
             <div style={style.icon}>
